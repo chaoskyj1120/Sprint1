8596a9711288ab32b6c7d0cd38b1669ad838365e
KwonYongJin <chaoskyj@gmail.com>
2025-07-15 13:07:52 +0900

    최신화

--- src/main/java/com/codeit/discodeit/DiscodeitApplication.java
+++ src/main/java/com/codeit/discodeit/DiscodeitApplication.java
@@ -2,12 +2,10 @@ package com.codeit.discodeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiscodeitApplication {
-	public static final String pathStaticFolder = "./src/main/resources/static/";
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
+		SpringApplication.run(DiscodeitApplication.class, args);
 	}
 }

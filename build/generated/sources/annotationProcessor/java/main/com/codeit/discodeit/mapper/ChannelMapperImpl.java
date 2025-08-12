41dbf57209b16883a440693015c1d7bedba2c7f7
KwonYongJin <chaoskyj@gmail.com>
2025-08-12 18:48:29 +0900

    feat: Actuator 적용

--- build/generated/sources/annotationProcessor/java/main/com/codeit/discodeit/mapper/ChannelMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/codeit/discodeit/mapper/ChannelMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-12T17:21:49+0900",
+    date = "2025-08-12T18:40:56+0900",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.14.3.jar, environment: Java 17.0.15 (Amazon.com Inc.)"
 )
 @Component

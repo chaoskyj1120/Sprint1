88787c6f902eb5c629574a99b751510212f8c84b
KwonYongJin <chaoskyj@gmail.com>
2025-08-12 18:15:01 +0900

    feat: 리퀘스트에 검증 어노테이션 추가

--- src/main/java/com/codeit/discodeit/dto/user_service_dto/UserUpdateRequest.java
+++ src/main/java/com/codeit/discodeit/dto/user_service_dto/UserUpdateRequest.java
@@ -1,7 +1,11 @@
 package com.codeit.discodeit.dto.user_service_dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,24 +13,26 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Schema(description = "수정할 User 정보")
 public class UserUpdateRequest {
 
   @Schema(description = "수정할 사용자 ID")
   private UUID userId;
 
+  @NotBlank
+  @Size(min = 4, max = 14)
   @Schema(description = "새 사용자 이름", example = "newUsername123")
   private String newUsername;
 
-  @Schema(description = "새 이메일 주소", example = "new.email@example.com")
+  @NotBlank
+  @Size(min = 10, max = 30)
+  @Email
+  @Schema(description = "새 이메일 주소", example = "newEmail@example.com")
   private String newEmail;
 
+  @NotBlank
+  @Size(min = 4, max = 14)
   @Schema(description = "새 비밀번호", example = "newSecurePassword123!")
   private String newPassword;
-
-  public UserUpdateRequest(String newUsername, String newEmail, String newPassword) {
-    this.newUsername = newUsername;
-    this.newEmail = newEmail;
-    this.newPassword = newPassword;
-  }
 }
--- src/main/java/com/codeit/discodeit/dto/user_status_dto/UserStatusUpdateRequest.java
+++ src/main/java/com/codeit/discodeit/dto/user_status_dto/UserStatusUpdateRequest.java
@@ -2,15 +2,13 @@ package com.codeit.discodeit.dto.user_status_dto;
 
 import java.time.Instant;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserStatusUpdateRequest {
 
   private Instant newLastActiveAt;
-
-  // getter, setter
-  public UserStatusUpdateRequest() {
-  }
 }

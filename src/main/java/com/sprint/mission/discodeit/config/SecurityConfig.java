abdedec2fb8b7aa0fbe862f46987d4a143bcd0d1
KwonYongJin <chaoskyj@gmail.com>
2025-10-16 14:28:04 +0900

    feat: 기본요구 사항 적용

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -1,22 +1,22 @@
 package com.sprint.mission.discodeit.config;
 
 import com.sprint.mission.discodeit.entity.Role;
-import com.sprint.mission.discodeit.entity.UserStatus;
+import com.sprint.mission.discodeit.handler.JwtLoginSuccessHandler;
+import com.sprint.mission.discodeit.handler.JwtLogoutHandler;
 import com.sprint.mission.discodeit.handler.LoginFailureHandler;
-import com.sprint.mission.discodeit.handler.LoginSuccessHandler;
-import com.sprint.mission.discodeit.handler.LogoutSuccessHandler;
 import com.sprint.mission.discodeit.handler.SpaCsrfTokenRequestHandler;
+import com.sprint.mission.discodeit.jwt.JwtAuthenticationFilter;
+import com.sprint.mission.discodeit.jwt.JwtProvider;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.DiscodeitUserDetailsService;
 import jakarta.servlet.http.HttpServletResponse;
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
@@ -26,30 +26,33 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-
 import com.sprint.mission.discodeit.entity.User;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.stereotype.Component;
+
 
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
-  private final LoginSuccessHandler loginSuccessHandler;
+  private final JwtLoginSuccessHandler jwtLoginSuccessHandler;
+  private final JwtLogoutHandler jwtLogoutHandler;
   private final LoginFailureHandler loginFailureHandler;
-  private final LogoutSuccessHandler logoutSuccessHandler;
   private final UserRepository userRepository;
 
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain filterChain(HttpSecurity http,
+      DiscodeitUserDetailsService discodeitUserDetailsService, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
     http
         .csrf(csrf -> csrf
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
@@ -62,7 +65,7 @@ public class SecurityConfig {
             .requestMatchers(HttpMethod.POST,
                 "/api/auth/login",
                 "/api/auth/logout",
-                "/api/users").permitAll()
+                "/api/users", "/api/auth/refresh").permitAll()
             .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll() // actuator
             .requestMatchers(
                 "/swagger-ui.html",
@@ -74,26 +77,24 @@ public class SecurityConfig {
             .loginProcessingUrl("/api/auth/login")
             .usernameParameter("username")
             .passwordParameter("password")
-            .successHandler(loginSuccessHandler)
+            .successHandler(jwtLoginSuccessHandler)
             .failureHandler(loginFailureHandler)
         )
         .logout(logout -> logout
             .logoutUrl("/api/auth/logout")
-            .logoutSuccessHandler(logoutSuccessHandler))
+            .addLogoutHandler(jwtLogoutHandler))
         .exceptionHandling(e -> e
             .defaultAuthenticationEntryPointFor(
                 new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
                 new AntPathRequestMatcher("/api/**")
             )
-            .accessDeniedHandler((req, res, ex) -> res.sendError(HttpServletResponse.SC_FORBIDDEN))
-        );
+            .accessDeniedHandler((req, res, ex) -> res.sendError(HttpServletResponse.SC_FORBIDDEN)))
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    ;
 
-
-    //Csrf Token GET
-    //회원가입  Post
-    //로그인   Post
-    //로그아웃 POST
-    //API가 아닌 요청(Swagger, Actuator 등)
     return http.build();
   }
 
@@ -122,7 +123,6 @@ public class SecurityConfig {
 
       User admin = new User(name, email, passwordEncoder.encode(rawPw), null, Role.ADMIN);
       Instant now = Instant.now();
-      UserStatus userStatus = new UserStatus(admin, now);
       userRepository.save(admin);
 
 
@@ -142,5 +142,23 @@ public class SecurityConfig {
     DefaultMethodSecurityExpressionHandler handler = new DefaultMethodSecurityExpressionHandler();
     handler.setRoleHierarchy(roleHierarchy);
     return handler;
+  }
+
+  @Bean
+  public SessionRegistry sessionRegistry() {
+    return new SessionRegistryImpl();
+  }
+
+  @Bean
+  public static HttpSessionEventPublisher httpSessionEventPublisher() {
+    return new HttpSessionEventPublisher();
+  }
+
+  @Bean
+  public JwtAuthenticationFilter jwtAuthenticationFilter(
+      JwtProvider jwtProvider,
+      DiscodeitUserDetailsService discodeitUserDetailsService
+  ) {
+    return new JwtAuthenticationFilter(jwtProvider, discodeitUserDetailsService);
   }
 }
--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -2,22 +2,28 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.UserRoleUpdateRequest;
+import com.sprint.mission.discodeit.jwt.JwtDto;
+import com.sprint.mission.discodeit.jwt.JwtProvider;
 import com.sprint.mission.discodeit.service.AuthService;
 import com.sprint.mission.discodeit.service.DiscodeitUserDetails;
+import com.sprint.mission.discodeit.service.DiscodeitUserDetailsService;
+import com.sprint.mission.discodeit.service.RefreshTokenService;
+import io.jsonwebtoken.Jwt;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,6 +34,8 @@ import lombok.extern.slf4j.Slf4j;
 public class AuthController{
 
   private final AuthService authService;
+  private final RefreshTokenService refreshTokenService;
+  private final JwtProvider jwtProvider;
 
   @GetMapping("csrf-token")
   public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
@@ -37,13 +45,6 @@ public class AuthController{
     return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).body(null);//203
   }
 
-  @GetMapping("/me")
-  public ResponseEntity<UserDto> getCurrentUser(@AuthenticationPrincipal DiscodeitUserDetails userDetails) {
-    // JSSON ID가 저장되어 있는데 거기사 ID를 가져와 userDetails를 가져옴
-    UserDto userDto = userDetails.getUserDto();
-    return ResponseEntity.ok(userDto);
-  }
-
   @PutMapping(value = "/role", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<UserDto> updateRole(@RequestBody UserRoleUpdateRequest userUpdateRequest) {
@@ -51,4 +52,20 @@ public class AuthController{
     UserDto userDto = authService.patchRole(userUpdateRequest);
     return ResponseEntity.ok(userDto);
   }
+
+  @PostMapping("/refresh")
+  public ResponseEntity<JwtDto> refresh(HttpServletRequest request, HttpServletResponse response) {
+    Cookie[] list = request.getCookies();
+    String oldRefreshToken = null;
+
+    for(Cookie cookie:list) {
+      if (cookie.getName().equals("REFRESH_TOKEN")) {
+        oldRefreshToken = cookie.getValue();
+      }
+    }
+
+    JwtDto jwtDto = refreshTokenService.validateRefreshTokenAndReissueAccessToken(oldRefreshToken, response); // 리프레쉬 토큰 재발급
+    return ResponseEntity.ok(jwtDto);
+  }
+
 }

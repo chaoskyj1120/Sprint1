1b799cf17b9ea2a688a74666fd1ed9f80d8f133e
KwonYongJin <chaoskyj@gmail.com>
2025-11-06 11:51:04 +0900

    fix: remove submodule and add base project as normal folder

---
+++ 0-sprint-mission-s11-v2-base/src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.sprint.mission.discodeit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.enableSimpleBroker("/sub");
+    registry.setApplicationDestinationPrefixes("/pub");
+  }
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    // 클라이언트가 연결할 WebSocket 엔드포인트 정의
+    registry.addEndpoint("/ws")
+        .setAllowedOriginPatterns("*") // CORS 허용
+        .withSockJS();
+  }
+
+}
